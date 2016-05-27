02440a5fe0b8496e2c03b815909688db7a8a3bfc
ashsha <ph@sf.local>
2016-05-27 11:45:26 +0300

    5

--- run.sh
+++ run.sh
@@ -4,5 +4,4 @@ VERSION="1.0.0.${BUILD_NUMBER:-99}"
 IMAGE=seth0/test_git
 ID=$(docker build  -t test_git  .  | tail -1 | sed 's/.*Successfully built \(.*\)$/\1/')
 docker tag ${ID} ${IMAGE}:${VERSION}
-docker tag ${ID} ${IMAGE}:latest
 docker push ${IMAGE}:${VERSION}
